f63fd91835bf488b93fd4e47783fba324a65ac3f
Edwin Mwandagha <eduzmwandagha@gmail.com>
2022-07-04 05:59:44 -0700

    alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**                                                                                                                                     
+ * main -Entry Point                                                                                                                  
+ *
+ * Return: Always 0 (success)                                                                                                           
+ */                                                                                                                                     
+
+int main(void)                                                                                                                          
+{                                                                                                                                       	char letter;
+	
+ 	for (letter = 'a'; letter <= 'z'; low++)
+		putchar(low);
+	for (letter = 'a'; letter <= 'z'; low++)
+		putchar(low);
+     	putchar('\n');
+       
+	return (0);                                                                                     
+}                                                                                                                                       
