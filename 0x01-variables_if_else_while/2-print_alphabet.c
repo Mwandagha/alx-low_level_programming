e9d768d234623df4052e223fe60ce894ba9b6cbc
Edwin Mwandagha <eduzmwandagha@gmail.com>
2022-07-04 05:20:46 -0700

    update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
+ *main -Entry Point
  *
- *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	char letter;
-	for(letter = 'a'; letter <= 'z'; letter++)
+	   for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 	putchar('\n');
 	return (0);
