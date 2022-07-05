e8429757c17f652c96adb9a6e59acc956a45451a
Edwin Mwandagha <eduzmwandagha@gmail.com>
2022-07-05 12:53:42 -0700

    putchar

---
+++ 0x02-functions_nested_loop/0-putchar.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *
+ *  * main - main function
+ *
+ *   *
+ *
+ *    * Return: End program
+ *
+ *     */
+ main(void)
+
+{
+		char string[8] = "_putchar";
+
+		int n = 0;
+		while (n < 8)
+		{
+		_putchar(string[n]);
+		++n;
+												}
+		_putchar(10);
+
+		return (0);
+
+}
