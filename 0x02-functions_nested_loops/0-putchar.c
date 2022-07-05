c44a9b72e7f91a6d37109a4d9ec2e415a2755833
Edwin Mwandagha <eduzmwandagha@gmail.com>
2022-07-05 15:05:08 -0700

    o_putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
- *  * main - prints Putchar
+ * main - prints Putchar
  *Description: prints _putchar
  *Return: 0 (Success)
  */
+
 int main(void)
 {
 	_putchar('_');
@@ -15,5 +17,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+	
 	return (0);
 }
