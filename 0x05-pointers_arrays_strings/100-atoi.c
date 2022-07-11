d64c1350326b37c99b314bf58d31392ec956ec93
Edwin Mwandagha <eduzmwandagha@gmail.com>
2022-07-11 14:32:39 -0700

    _atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -34,21 +34,12 @@ int _atoi(char *s)
 		}
 
 		count++;
-
 	}
-
-
-
 	for (i = count - size; i < count; i++)
-
 	{
-
 		oi = oi + ((*(s + i) - 48) * m);
-
 		m /= 10;
-
 	}
-
 	return (oi * pn);
 
 }
