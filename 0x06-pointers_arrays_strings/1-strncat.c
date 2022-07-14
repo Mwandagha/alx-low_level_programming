e3899e2f1c58247c63387ba533a3fed05046f01d
Edwin Mwandagha <eduzmwandagha@gmail.com>
2022-07-14 12:02:50 -0700

    cat function

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,17 +1,17 @@
 #include "main.h"
+
 /**
- * _strcat - concatenates two strings,
+ * _strncat - concatenates two strings,
  * @dest: destination.
  * @src: source.
+ * @n: amount of bytes used from src.
  * Return: the pointer to dest.
  */
-
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int count = 0, count2 = 0;
 
 	while (*(dest + count) != '\0')
-
 	{
 
 		count++;
@@ -19,8 +19,7 @@ char *_strcat(char *dest, char *src)
 	}
 
 
-
-	while (count2 >= 0)
+	while (count2 < n)
 
 	{
 
