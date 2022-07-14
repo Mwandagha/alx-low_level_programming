882952b78f2cedd8163bb0f57fbfe064fdcd7643
Edwin Mwandagha <eduzmwandagha@gmail.com>
2022-07-14 12:58:15 -0700

    print buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,5 +15,6 @@ int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
