c968c2f387e2a69a1c3a844621ddd1ddb8d966ce
Edwin Mwandagha <eduzmwandagha@gmail.com>
2022-07-26 12:13:03 -0700

    strup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,24 +1,29 @@
- * @size: size of the array.
- * @c: storaged char
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory.
+ * @str: string.
  *
  * Return: pointer of an array of chars
  */
-char *create_array(unsigned int size, char c)
+char *_strdup(char *str)
 {
-        char *cr;
-        unsigned int i;
+	char *strout;
+	unsigned int i, j;
 
-        if (size == 0)
-                return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-        cr = malloc(sizeof(c) * size);
+	for (i = 0; str[i] != '\0'; i++)
+		;
 
-        if (cr == NULL)
-                return (NULL);
+	strout = (char *)malloc(sizeof(char) * (i + 1));
 
-        for (i = 0; i < size; i++)
-                cr[i] = c;
+	if (strout == NULL)
+		return (NULL);
 
-        return (cr);
+	for (j = 0; j <= i; j++)
+		strout[j] = str[j];
+
+	return (strout);
 }
-
